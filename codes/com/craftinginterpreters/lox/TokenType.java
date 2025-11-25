56fc61b705a7ea3b31abbb1b62e6b3efd234f964
cimn23009-stack <cimn23009@g.nihon-u.ac.jp>
2025-11-25 11:53:10 +0900

    add LOX.java and TokenType.java

---
+++ codes/com/craftinginterpreters/lox/TokenType.java
@@ -0,0 +1,14 @@
+package com.craftinginterpreters.lox;
+
+public enum TokenType {
+  // Single-character tokens.
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BARCE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+  // One or two character tokens.
+  BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+  // Literals.
+  IDENTIFIER, STRING, NUMBER,
+  // Keywords.
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+  EOF
+}
