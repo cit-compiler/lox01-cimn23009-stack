4f63812fa78e3f6237d1198951ecb74f170ebdc0
cimn23009-stack <cimn23009@g.nihon-u.ac.jp>
2025-12-02 10:26:32 +0900

    add string

--- codes/com/craftinginterpreters/lox/Scanner.java
+++ codes/com/craftinginterpreters/lox/Scanner.java
@@ -62,6 +62,8 @@ public class Scanner {
         line++;
         break;
 
+      case '"': string(); break;
+
       default:
         Lox.error(line, "Unexpected character.");
         break;
@@ -96,5 +98,22 @@ public class Scanner {
   private char peek(){
     if(isAtEnd()) return '\0';
     return source.charAt(current);
+  }
+
+  private void string(){
+    while (peek() != '"' && !isAtEnd()) {
+      if(peek() ==  '\n') line++;
+      advance();
+    }
+
+    if(isAtEnd()){
+      Lox.error(line, "Unterminated string.");
+      return;
+    }
+
+    advance();
+
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
   }
 }
