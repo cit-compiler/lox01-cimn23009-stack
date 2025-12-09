30dc156046186f2ff5919047bd7591d3e46e1f1f
cimn23009-stack <cimn23009@g.nihon-u.ac.jp>
2025-12-09 11:19:50 +0900

    add GenerateAST.java and Generated Expr.java

--- codes/com/craftinginterpreters/lox/Scanner.java
+++ codes/com/craftinginterpreters/lox/Scanner.java
@@ -10,6 +10,28 @@ import static com.craftinginterpreters.lox.TokenType.*;
 public class Scanner {
   private final String source;
   private final List<Token> tokens = new ArrayList<>();
+  private static final Map<String, TokenType> keywords;
+
+  static{
+    keywords = new HashMap<>();
+    keywords.put("and", AND);
+    keywords.put("class", CLASS);
+    keywords.put("else", ELSE);
+    keywords.put("false", FALSE);
+    keywords.put("for", FOR);
+    keywords.put("fun", FUN);
+    keywords.put("if", IF);
+    keywords.put("nil", NIL);
+    keywords.put("or", OR);
+    keywords.put("print", PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super", SUPER);
+    keywords.put("this", THIS);
+    keywords.put("true", TRUE);
+    keywords.put("var", VAR);
+    keywords.put("while", WHILE);
+  }
+
   private int start = 0;
   private int current = 0;
   private int line = 1;
@@ -28,7 +50,7 @@ public class Scanner {
     return tokens;
   }
 
-  private void scanTokens(){
+  private void scanToken(){
     char c = advance();
     switch (c) {
       case '(': addToken(LEFT_PAREN); break;
@@ -52,6 +74,11 @@ public class Scanner {
           addToken(SLASH);
         }
         break;
+      case 'o':
+        if(match('r')){
+          addToken(OR);
+        }
+        break;
         
       case ' ':
       case '\r':
@@ -65,9 +92,71 @@ public class Scanner {
       case '"': string(); break;
 
       default:
-        Lox.error(line, "Unexpected character.");
+        if(isDigit(c)){
+          number();
+        }else if(isAlpha(c)){
+          identifier();
+        }else{
+          Lox.error(line, "Unexpected character.");
+        }
         break;
     }
+  }
+
+  private void identifier(){
+    while(isAlphaNumberic(peek())) advance();
+
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if(type == null) type = IDENTIFIER;
+    addToken(type);
+  }
+  
+  private void string(){
+    while (peek() != '"' && !isAtEnd()) {
+      if(peek() ==  '\n') line++;
+      advance();
+    }
+    
+    if(isAtEnd()){
+      Lox.error(line, "Unterminated string.");
+      return;
+    }
+    
+    advance();
+    
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
+  }
+
+  private boolean match(char expected){
+    if(isAtEnd()) return false;
+    if(source.charAt(current) != expected) return false;
+
+    current++;
+    return true;
+  }
+
+  private char peek(){
+    if(isAtEnd()) return '\0';
+    return source.charAt(current);
+  }
+
+  private char peekNext(){
+    if(current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  }
+
+  private boolean isAlpha(char c){
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+  }
+
+  private boolean isAlphaNumberic(char c){
+    return isAlpha(c) || isDigit(c);
+  }
+  
+  private boolean isDigit(char c){
+    return c >= '0' && c <= '9';
   }
 
   private boolean isAtEnd(){
@@ -87,33 +176,13 @@ public class Scanner {
     tokens.add(new Token(type, text, literal, line));
   }
 
-  private boolean match(char expected){
-    if(isAtEnd()) return false;
-    if(source.charAt(current) != expected) return false;
-
-    current++;
-    return true;
-  }
-
-  private char peek(){
-    if(isAtEnd()) return '\0';
-    return source.charAt(current);
-  }
-
-  private void string(){
-    while (peek() != '"' && !isAtEnd()) {
-      if(peek() ==  '\n') line++;
+  private void number(){
+    while (isDigit(peek())) advance();
+    if(peek() == '.' && isDigit(peekNext())){
       advance();
+      while(isDigit(peek())) advance();
     }
 
-    if(isAtEnd()){
-      Lox.error(line, "Unterminated string.");
-      return;
-    }
-
-    advance();
-
-    String value = source.substring(start + 1, current - 1);
-    addToken(STRING, value);
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
   }
 }
